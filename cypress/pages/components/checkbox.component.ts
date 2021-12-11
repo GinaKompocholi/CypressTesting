5750169fa849e4f5117ac04a4eda6845344f8ce8
GinaKompocholi <gina_holly@yahoo.gr>
2021-12-11 17:32:07 +0200

    component cleanup

--- cypress/pages/components/checkbox.component.ts
+++ cypress/pages/components/checkbox.component.ts
@@ -1,25 +1,3 @@
-/* mesa sto component de thelw tests
-cy.contains('Powered by ') px auto k to should exist
-to component exei ta xaraktiristika
-
-ti borw na kanw sti checkbox page?
-posa checkboxes exw, ta checkarw, uncheck
-auta einai tou component
-1. 
-checkboxPage.page k kalw methodo px select checkbox
-2. 'h
-yparxei sa methodos sto checkboxPage.page k kalei ti check methodo tou component
-
------
-ta test actions prp na pane sto spec
-ta standard checks
-- pare footer
-- pare header
-- pare url
-na klironomountai apo tin Page
-(general page)
-px Homepage.page extends Page
-*/
 class CheckboxComponent{
 
     
@@ -34,16 +12,14 @@ class CheckboxComponent{
     public getCheckboxElement(checkboxLocator: number | Cypress.Chainable){
         return cy.get(`#checkboxes > :nth-child(${checkboxLocator})`);
     }
-    
+
+    // get and .check()
     public findAndCheckCheckboxElement(dropdownLocator: number | Cypress.Chainable){
         this.getCheckboxElement(dropdownLocator).check()
     }
+    // just .check()
     public checkCheckboxElement(checkboxElement: Cypress.Chainable){
         checkboxElement.check()
-    }
-
-    public uncheckCheckbox(dropdownLocator){
-        dropdownLocator.uncheck
     }
 }
 
